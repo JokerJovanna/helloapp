2573e02b26f41c23cde6be25cf5a669c844ac2fc
white <white@example.com>
2025-10-28 14:33:44 +0500

    Add HelloWhite method

--- AppForGit/Program.cs
+++ AppForGit/Program.cs
@@ -4,6 +4,11 @@ namespace HelloApp
 {
     class Program
     {
+        public static void greetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
+
         static void Main()
         {
             Console.WriteLine("hello");
